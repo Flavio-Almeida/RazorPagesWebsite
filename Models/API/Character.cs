99befd0e2c49559ddc286f0fe7e89ddc111a2237
Flavio Almeida <katsugo@hotmail.com>
2024-05-20 19:41:33 -0300

    Adding the models folder and the class Character

---
+++ Models/API/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RazorPagesWebsite.Models.API
+{
+    public class Character
+    {
+        [JsonPropertyName("")]
+        public required int Id { get; set; }
+
+        [JsonPropertyName("")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("")]
+        public string? Status { get; set; }
+
+        [JsonPropertyName("")]
+        public string? Species { get; set; }
+
+        [JsonPropertyName("")]
+        public string? Gender { get; set; }
+
+        [JsonPropertyName("")]
+        public string? Image { get; set; }
+
+    }
+}
